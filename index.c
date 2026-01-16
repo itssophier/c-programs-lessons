059c35f1daac293ecefdc82a5a934e5411ede0f6
its.sofear <144211327+itssophier@users.noreply.github.com>
2026-01-16 14:35:19 +0300

    Add files via upload

---
+++ index.c
@@ -0,0 +1,17 @@
+//Hello World program
+/*
+ Description: a program to display hello world
+ Name: Sofia Juma 
+ Reg no: BCS-03-0127/2025
+ DATE:16/1/2025
+ Version 1
+ */
+
+#include <stdio.h>
+
+int main() {
+	//display hello world
+	printf("Hello World");
+	
+	return 0; //successful execution 
+}
