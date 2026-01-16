74f6578fcb0c96d6a52bd3fe53f30aa6e0f98d34
its.sofear <144211327+itssophier@users.noreply.github.com>
2026-01-16 14:39:03 +0300

    Add files via upload
    this is a program to display my name

---
+++ name.c
@@ -0,0 +1,17 @@
+//Hello World program
+/*
+ Description: a program to display my name 
+ Name: Sofia Juma 
+ Reg no: BCS-03-0127/2025
+ DATE:16/1/2025
+ Version 1
+ */
+
+#include <stdio.h>
+
+int main() {
+	//display my name
+	printf("Sofia Juma");
+	
+	return 0; //successful execution 
+}
